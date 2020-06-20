52a5c6fab431e43b1613d235b1c1490623cafd98
smartmajid <67175399+smartmajid@users.noreply.github.com>
2020-06-19 19:07:23 -0700

    Update Michael.js

--- Michael.js
+++ Michael.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'NzE1NTI1NjM4ODg2MzkxODI5.XuzysA.xk6SuY3qwnhfDSUyMf18SO8pOoo';
-const PREFIX = '>';
+const token = 'NzE1NTI1NjM4ODg2MzkxODI5.Xu1vNg.adFzgV1oeW_FZnmX3ubv5tdrkEc';
+const PREFIX = '+';
 
 client.on('ready', () => {
 	console.log('Nelz is online!')
@@ -11,3 +11,44 @@ client.on('ready', () => {
 		}
 	});
 })
+
+client.on('message', async(message) => {
+	try {
+		if (!message.guild || message.author.bot) return;
+		if (!message.content.startsWith(PREFIX)) return;
+		const args = message.content.split(' ');
+		if (!args[0]) return;
+		const command = args.shift().slice(PREFIX.length).toLowerCase();
+		if (command === 'ping') {
+			await message.channel.send(`Ping is ${Math.round(client.ping)}ms`);
+		}
+		if (command === 'ban') {
+			if (!message.member.hasPermission('BAN_MEMBERS')) {
+				await message.channel.send('You have no permissions to do this command');
+				return;
+			}
+			if (!message.mentions.members.size) {
+				await message.channel.send('You did not mention anyone');
+				return;
+			}
+			const member = message.mentions.members.first();
+			await member.ban('He was being bad');
+		}
+		if (command === 'kick') {
+			if (!message.member.hasPermission('KICK_MEMBERS')) {
+				await message.channel.send('You have no permissions to do this command');
+				return;
+			}
+			if (!message.mentions.members.size) {
+				await message.channel.send('You did not mention anyone');
+				return;
+			}
+			const member = message.mentions.members.first();
+			await member.kick('He was being bad');
+		}
+	} catch (error) {
+		console.error(error);
+	}
+
+})
+client.login(NzE1NTI1NjM4ODg2MzkxODI5.Xu1vNg.adFzgV1oeW_FZnmX3ubv5tdrkEc);
